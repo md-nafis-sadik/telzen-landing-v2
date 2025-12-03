95681824af365b584885b31af70f40248e769750
md-nafis-sadik <sadikmdnafis@gmail.com>
2025-12-03 17:42:47 +0600

    prefetch and small fixes

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,7 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     // Enable prefetching in development
-    optimizePackageImports: ['@reduxjs/toolkit', 'motion'],
-  },
-  // Optimize prefetching and caching
-  onDemandEntries: {
-    // period (in ms) where the server will keep pages in the buffer
-    maxInactiveAge: 60 * 1000,
-    // number of pages that should be kept simultaneously without being disposed
-    pagesBufferLength: 5,
+    optimizePackageImports: ["@reduxjs/toolkit", "motion"],
   },
 };
 
